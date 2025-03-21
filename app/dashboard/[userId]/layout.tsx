c7259267ce6e567d1cf334f9d9e62caf95ef6976
Utkarsh Raj Mishra <utkarshrajmishra811545@gmail.com>
2025-03-21 20:24:50 +0530

    Add: Shadcn Dashboard

--- app/dashboard/[userId]/layout.tsx
+++ app/dashboard/[userId]/layout.tsx
@@ -1,16 +1,8 @@
 import type React from "react";
-import Sidebar from "./@sidebar/page";
-import Script from "@/components/Script";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="h-[100vh] w-full bg-gradient-to-b from-neutral-950 to-black flex px-6">
-      <Sidebar />
-      <div className="p-6 h-screen overflow-y-auto">
-        <Script/>
-        {children}</div>
-    </div>
-  );
+  return;
+  <div>{children}</div>;
 };
 
 export default Layout;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,14 +9,17 @@ export default {
   ],
   theme: {
   	extend: {
-		fontFamily:{
-		inter:['Inter', 'sans-serif']
-		},
+  		fontFamily: {
+  			inter: [
+  				'Inter',
+  				'sans-serif'
+  			]
+  		},
   		colors: {
-			pink:'#DC3DA8',
-			purple:'#7C26EC',
-			bgColor:'#080708',
-			green:'#20E1A3',
+  			pink: '#DC3DA8',
+  			purple: '#7C26EC',
+  			bgColor: '#080708',
+  			green: '#20E1A3',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -56,6 +59,16 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
+  			},
+  			sidebar: {
+  				DEFAULT: 'hsl(var(--sidebar-background))',
+  				foreground: 'hsl(var(--sidebar-foreground))',
+  				primary: 'hsl(var(--sidebar-primary))',
+  				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
+  				accent: 'hsl(var(--sidebar-accent))',
+  				'accent-foreground': 'hsl(var(--sidebar-accent-foreground))',
+  				border: 'hsl(var(--sidebar-border))',
+  				ring: 'hsl(var(--sidebar-ring))'
   			}
   		},
   		animation: {
